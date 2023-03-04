281b04fd28234ffc824d6b6c6969a75ea770a19e
Abegemot <marcha64@yahoo.com>
2023-03-04 17:43:18 +0100

    04/03/2023 KResult, createBook, executeSrv

--- sharedlibrary/build.gradle.kts
+++ sharedlibrary/build.gradle.kts
@@ -4,60 +4,66 @@ plugins {
 
 group ="com.begemota"
 
-val KotlinVer ="1.6.21"
-val jcompose  ="1.2.0-rc01" //   "1.1.1"
+val KotlinVer ="1.7.20"
+val jcompose  ="1.3.0" //   "1.1.1"
+
 
 dependencies {
+    //classpath("com.android.tools.build:gradle:7.3.0")
     constraints {
         api("org.jetbrains.kotlin.jvm:$KotlinVer")
 
         api("org.jetbrains.kotlin:kotlin-stdlib$KotlinVer")
 
-        api("androidx.compose.material:material-icons-extended:$jcompose")
-        api("androidx.compose.runtime:runtime:$jcompose")
-        api("androidx.compose.material:material:$jcompose")
-        api("androidx.compose.ui:ui-tooling:$jcompose")
-        api("androidx.compose.ui:ui-util:$jcompose")
-        api("androidx.compose.compiler:compiler:$jcompose")
-        api("androidx.compose.foundation:foundation:$jcompose")
-        api("androidx.compose.foundation:foundation-layout:$jcompose")
+        //api("androidx.compose.material:material-icons-extended:$jcompose")
+        //api("androidx.compose.runtime:runtime:$jcompose")
+        //api("androidx.compose.material:material:$jcompose")
+        //api("androidx.compose.ui:ui-tooling:$jcompose")
+        //api("androidx.compose.ui:ui-util:$jcompose")
+        //api("androidx.compose.compiler:compiler:1.3.2")
+        //api("androidx.compose.foundation:foundation:$jcompose")
+        //api("androidx.compose.foundation:foundation-layout:$jcompose")
 
-        api("androidx.customview:customview:1.1.0")
-        api("androidx.customview:customview-poolingcontainer:1.0.0-beta02")
+        //api("androidx.customview:customview:1.1.0")
+        //api("androidx.customview:customview-poolingcontainer:1.0.0")
 
-        api("androidx.activity:activity-compose:1.4.0")
-        api("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+        api("androidx.activity:activity-compose:1.6.1")
+        api("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 
 
-        api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
-        api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.3")
+        api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+        api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
         api("org.jetbrains.kotlin:kotlin-reflect:$KotlinVer")
 
        //plugins
-        api("com.android.tools.build:gradle:7.2.1")
+        api("com.android.tools.build:gradle:7.4.2")
         api("org.jetbrains.kotlin:kotlin-gradle-plugin:$KotlinVer")
         api("org.jetbrains.kotlin.android:$KotlinVer")
-        api("com.github.ben-manes.versions:0.42.0")
+        api("com.github.ben-manes.versions:0.44.0")
         //
         api("com.sun.mail:android-mail:1.6.7")
         api("com.sun.mail:android-activation:1.6.7")
 
-        api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.3")
-        api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+        api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.1")
+        api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
         api("com.jakewharton.timber:timber:5.0.1")
-        api("io.github.microutils:kotlin-logging-jvm:2.1.23")
-        api("com.squareup.leakcanary:leakcanary-android:2.9.1")
+        api("io.github.microutils:kotlin-logging-jvm:3.0.5")
+        api("com.squareup.leakcanary:leakcanary-android:2.10")
 
-        api("com.google.cloud:google-cloud-logging-logback:0.123.1-alpha")
-        api("com.google.cloud:google-cloud-storage:2.8.1")
+        api("com.google.cloud:google-cloud-logging-logback:0.130.0-alpha")
+        api("com.google.cloud:google-cloud-storage:2.19.0")
         api("com.google.android.play:core:1.10.3")
         api("com.google.android.play:core-ktx:1.8.1")
-        api("ch.qos.logback:logback-classic:1.2.3")
+        api("ch.qos.logback:logback-classic:1.4.5")
 
-        val KtorVer                       = "2.0.2"
+        val KtorVer                       = "2.2.3"
+        api("io.ktor:ktor-client-core:$KtorVer")
+        api("io.ktor:ktor-client-okhttp:$KtorVer")
         api("io.ktor:ktor-client-cio:$KtorVer")
+        api("io.ktor:ktor-client-android:$KtorVer")
         api("io.ktor:ktor-client-logging-jvm:$KtorVer")
+        api("io.ktor:ktor-client-logging:$KtorVer")
         api("io.ktor:ktor-client-encoding:$KtorVer")
         api("io.ktor:ktor-serialization:$KtorVer")
         api("io.ktor:ktor-client-content-negotiation:$KtorVer")
@@ -68,7 +74,7 @@ dependencies {
         api("io.ktor:ktor-server-netty:$KtorVer")
         api("io.ktor:ktor-server-tests:$KtorVer")
 
-        api("org.jsoup:jsoup:1.15.1")
+        api("org.jsoup:jsoup:1.15.4")
 
         api("junit:junit:4.13.2")
         api("androidx.test.ext:junit:1.1.2")
